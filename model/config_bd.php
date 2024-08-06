c57f3fe8ad9ad52f86c8abbd82617ea5c8851244
regilanmeira <regilan@hotmail.com>
2024-08-06 14:58:33 -0300

    Adicionado o arquivo de configuração da base de dados

---
+++ model/config_bd.php
@@ -0,0 +1,48 @@
+<?php
+
+$servidor = "";
+$usuario = "";
+$senha = "";
+$bd = "";
+
+// Criando nova conexão 
+global $conn;
+$conn = mysqli_connect($servidor, $usuario, $senha, $bd);
+// Verificar a conexão 
+if (!$conn) {
+    die("Falha na conexão" . mysqli_connect_error());
+}
+
+function executarComando($sql) {
+    global $conn;
+    if (mysqli_query($conn, $sql)) {
+        return true;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        return false;
+    }
+}
+
+function executarComandoRetornarID($sql) {
+    global $conn;
+    if (mysqli_query($conn, $sql)) {
+        $ultimo_id = mysqli_insert_id($conn);
+        return $ultimo_id;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        return 0;
+    }
+}
+
+function retornarDados($sql) {
+    global $conn;
+    $resultado = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($resultado) > 0) {
+        return $resultado;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        //return 0;
+    }
+}
+?>
