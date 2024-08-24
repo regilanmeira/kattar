1464061c9134bd98344659fd41e1b95f324d2e9c
Rafael Sena <rafaelsenabarreto@gmail.com>
2024-08-24 18:04:50 -0300

    Criação da classe usuário

--- view/index.php
+++ view/index.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-<?php 
+
+    <?php
 
     include "../model/usuario.php";
 
-    $usuario  = new Usuario("ricardo@abc.com.br","Ricardo Santos","123456","7399999","BA","Itabuna","Não definido","Rua ABC");
+    $usuario  = new Usuario("ricardo@abc.com.br", "Ricardo Santos", "123456", "7399999", "BA", "Itabuna", "Não definido", "Rua ABC");
 
-    if ($usuario->criarUsuario() == true)
-    {
+    if ($usuario->criarUsuario() == true) {
         echo "Usuário criado";
-    }
-    else
-    {
+    } else {
         echo "Não foi possivel criar o usuário";
     }
 
-?>
+    ?>
+
 </body>
+
 </html>
