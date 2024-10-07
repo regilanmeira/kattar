202909f5dbb8f2de69f31153a0e482b121d4d4ea
Rafael Sena <rafaelsenabarreto@gmail.com>
2024-10-07 19:02:31 -0300

    Adicionando o controller do Tipo de Material

---
+++ controller/tipo_material_salvar.php
@@ -0,0 +1,25 @@
+<?php
+
+//Incluir o arquivo da classe tipo de material
+require "../model/tipo_material.php";
+
+include "referencias.php";
+
+//Capturar os elementos digitados no formulário
+
+$id = $_POST["txtId"];
+$descricao = $_POST["txtDescricao"];
+$observacao = $_POST["txtObservacao"];
+
+$tmat = new TipoMaterial($id,$descricao,$observacao);
+
+if ($tmat->criarTipoMaterial())
+{
+    echo "<h1 class='alert alert-success'>Tipo de material adicionada com sucesso!</h1>";
+}
+else
+{
+    echo "<h1 class='alert alert-danger'>Não foi possível adicionar o tipo de material. </h1> ";
+}
+
+?>
